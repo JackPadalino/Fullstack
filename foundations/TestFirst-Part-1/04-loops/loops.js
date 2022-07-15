43f2ec8d2131297a5faf8f74d46632c04c1bc7f0
JackPadalino <pada0867@gmail.com>
2022-07-14 20:20:33 -0400

    completed challenge problems for 04-loops

--- foundations/TestFirst-Part-1/04-loops/loops.js
+++ foundations/TestFirst-Part-1/04-loops/loops.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
-// repeat function
-repeat=(str,num)=>{
+//~~~~~repeat function~~~~~//
+const repeat=(str,num)=>{
     let newStr='';
     for(let i=0;i<num;i++){
         newStr+=str;
@@ -10,7 +10,7 @@ repeat=(str,num)=>{
 };
 
 // sum function
-sum=(arr)=>{
+const sum=(arr)=>{
     let sum=0;
     for(let i in arr){
         sum+=arr[i];
@@ -18,8 +18,8 @@ sum=(arr)=>{
     return sum;
 };
 
-// join function
-join=(arr,sep)=>{
+//~~~~~join function~~~~~//
+const join=(arr,sep)=>{
     let newStr='';
     for(let i=0;i<arr.length;i++){
         newStr+=arr[i];
@@ -30,20 +30,31 @@ join=(arr,sep)=>{
     return newStr;
 };
 
-/*
-join=(arr,sep)=>{
-    if(sep){
-        return arr.join(sep);
-    }else{
-        return arr.join();
+//~~~~~gridGenerator function~~~~~//
+
+// final gridGenerator code:
+const gridGenerator=(num)=>{
+    let grid='';
+    if(num>0){
+        for(let a=0;a<num;a++){
+            for(let b=0;b<num;b++){
+                if((a+b)%2==0){
+                    grid+='#';
+                }else{
+                    grid+=' ';
+                };
+            };
+            if(a<num){
+                grid+='\n';
+            };
+        };
     };
+    return grid;
 };
-*/
 
 /*
-// gridGenerator function - first attempt - only solves for
-// grids with odd nums
-gridGenerator=(num)=>{
+// first attempt at gridGenerator - only solves for grids with odd nums:
+const gridGenerator=(num)=>{
     let grid='';
     let counter=1;
     if(num>0){
@@ -65,21 +76,85 @@ gridGenerator=(num)=>{
 };
 */
 
-gridGenerator=(num)=>{
-    let grid='';
-    if(num>0){
-        for(let a=0;a<num;a++){
-            for(let b=0;b<num;b++){
-                if((a+b)%2==0){
-                    grid+='#';
-                }else{
-                    grid+=' ';
-                };
-            };
-            if(a<num){
-                grid+='\n';
-            };
+//~~~~~paramify function~~~~~//
+
+// final paramify code - using the join function (line 22):
+const paramify=(obj)=>{
+    let objArr=[];
+    for(let key in obj) {
+      if(obj.hasOwnProperty(key)){
+          objArr.push(`${key}=${obj[key]}`);
+      };
+    };
+    objArr.sort();
+    let finalObjStr=join(objArr,'&');
+    return finalObjStr;
+};
+
+/*
+// solving 'paramify' function without using the 'join' function from line 22:
+const paramify=(obj)=>{
+    let finalObjStr='';
+    let objArr=[];
+    for(let key in obj) {
+      if(obj.hasOwnProperty(key)){
+          objArr.push(`${key}=${obj[key]}`);
+      };
+    };
+    objArr.sort();
+    for(let i=0;i<objArr.length;i++){
+        finalObjStr+=objArr[i];
+        if(objArr.length>1 && i<objArr.length-1){
+            finalObjStr+='&';
+        }
+    };
+    return finalObjStr;
+};
+*/
+
+//~~~~~paramifyObjectKeys function~~~~~//
+
+// solving 'paramifyObjectKeys' function wit 'map' and 'join' (line 22) functions:
+const paramifyObjectKeys=(obj)=>{
+    const objArr = Object.keys(obj).map(key=>`${key}=${obj[key]}`);
+    objArr.sort();
+    let finalObjStr = join(objArr,'&');
+    return finalObjStr;
+};
+
+/*
+// solving 'paramifyObjectKeys' function without 'map' function:
+const paramifyObjectKeys=(obj)=>{
+    let finalObjStr = '';
+    const objArr = [];
+    const objKeys = Object.keys(obj);
+    const objValues = Object.values(obj);
+    for(let i=0;i<objKeys.length;i++){
+        objArr.push(`${objKeys[i]}=${objValues[i]}`)
+    };
+    objArr.sort();
+    for(let i in objArr){
+        finalObjStr+=objArr[i];
+        if(objKeys.length>1 && i<objKeys.length-1){
+            finalObjStr+='&';
         };
     };
-    return grid;
+    return finalObjStr;
+};
+*/
+
+//~~~~~sort function~~~~~//
+
+// solving 'sort' function with a selection sort algorithm:
+const sort=(arr)=>{
+    for(let a=0;a<arr.length-1;a++){
+        let index = a;
+        for(let b=a+1;b<arr.length;b++){
+            if(arr[b]<arr[index]){
+                index = b;
+            };
+        };
+        [arr[a], arr[index]] = [arr[index], arr[a]];
+    };
+    return arr;
 };
